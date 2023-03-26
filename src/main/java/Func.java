a63f976364927556c494915bfe47d852ae7c30ff
Sitkevich88 <Sitkevich2002@mail.ru>
2023-03-26 20:26:21 +0300

    Изменил Func.java

--- src/main/java/Func.java
+++ src/main/java/Func.java
@@ -1,4 +1,6 @@
 import org.example.Calculable;
+import org.example.logarithms.Ln;
+import org.example.logarithms.Log;
 import org.example.trigonometry.*;
 
 public class Func implements Calculable {
@@ -8,8 +10,9 @@ public class Func implements Calculable {
     Sec sec;
     Cos cos;
     Tan tan;
-
     Sin sin;
+    Ln ln;
+    Log log;
 
     public Func(){
         cot = new Cot();
@@ -18,19 +21,25 @@ public class Func implements Calculable {
         cos = new Cos();
         tan = new Tan();
         sin = new Sin();
+        ln = new Ln();
+        log = new Log();
     }
 
-    public Func(Cot cot, Csc csc, Sec sec, Cos cos, Tan tan, Sin sin){
+    public Func(Cot cot, Csc csc, Sec sec, Cos cos, Tan tan, Sin sin, Ln ln, Log log){
         this.cot = cot;
         this.csc = csc;
         this.sec = sec;
         this.cos = cos;
         this.tan = tan;
         this.sin = sin;
+        this.ln = ln;
+        this.log = log;
     }
 
 
     public double calc(double x, double eps){
+        if (!Double.isFinite(x)) return Double.NaN;
+        
         if (x <= 0){
             return ((Math.pow((((((cot.calc(x, eps) * csc.calc(x, eps)) * sec.calc(x, eps)) + cos.calc(x, eps)) +
                     (sec.calc(x, eps) / tan.calc(x, eps))) -
@@ -40,7 +49,10 @@ public class Func implements Calculable {
                             + (((sin.calc(x, eps) + sec.calc(x, eps)) + csc.calc(x, eps)) /
                             Math.pow(tan.calc(x, eps), 3))))) * csc.calc(x, eps));
         } else {
-            return 12345;
+            return Math.pow((
+                    ((ln.calc(x, eps) - log.calc(x, eps)) + (ln.calc(x, eps) / log.calc(5, x, eps)))
+                    / (ln.calc(x, eps) + log.calc(3, x, eps))
+            ), 2.0) * log.calc(2, x, eps);
         }
     }
 

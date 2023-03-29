36c6fcb2749cd36d72874d530c980f97cf75bd1f
SLNSL <nik231002@mail.ru>
2023-03-29 16:21:29 +0300

    csv output

---
+++ src/main/java/org/example/сsv/CSVPrint.java
@@ -0,0 +1,22 @@
+package org.example.сsv;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class CSVPrint {
+    public void csvPrint(double number, double answer, String path) throws FileNotFoundException {
+        PrintStream printStream = new PrintStream(new FileOutputStream(path, true));
+        printStream.printf("%s, %s \n", number, answer);
+    }
+
+    public void csvPrint(double number, double base, double answer, String path) throws FileNotFoundException {
+        PrintStream printStream = new PrintStream(new FileOutputStream(path, true));
+        printStream.printf("%s, %s, %s \n", number, base, answer);
+    }
+
+    public void csvPrint(String string, String path) throws FileNotFoundException {
+        PrintStream printStream = new PrintStream(new FileOutputStream(path, true));
+        printStream.printf("%s \n", string);
+    }
+}
